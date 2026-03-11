8780e20d3c01503b2b8792061835fc944a1013e9
Rahoof-labonte <rahoof@labonteapp.com>
2026-03-11 12:26:22 +0530

    conversion create and response schema

---
+++ backend/app/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+from datetime import date
+
+class ConversionCreate(BaseModel):
+
+    usd_amount: float
+    rate: float = Field(gt=0)
+    rate_source: str
+    conversion_date: date
+
+
+class ConversionResponse(BaseModel):
+
+    id: int
+    usd_amount: float
+    rate: float
+    jpy_amount: float
+    rate_source: str
+    conversion_date: date
+
+    class Config:
+        orm_mode = True
